0fc324ec8091c31918142bd7edf4cea3c7066d9d
Eduardo Hernández Villa <ehernandezvilla@gmail.com>
2024-02-22 23:52:18 -0300

    editable budget modification

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -59,7 +59,7 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                  <p> £</p>
                     <input
                         required='required'
                         type='number'
@@ -68,7 +68,7 @@ const AllocationForm = (props) => {
                         style={{ marginLeft: '2rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
-
+                    
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                         Save
                     </button>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,15 +2,23 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
+    const { dispatch } = useContext(AppContext)
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
+        const updatedBudget = event.target.value;
+        setNewBudget(updatedBudget);
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: Number(updatedBudget),
+        });
+        // console.log(budget)
+    };
+
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+    <span>Budget:£ </span>
+    <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
 };
