14bee32225e684adcb5443c51ab65a8c7c770808
Eduardo Hernández Villa <ehernandezvilla@gmail.com>
2024-02-23 23:28:08 -0300

    Currency component

---
+++ src/components/CurrencyForm.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import '../styles/CurrencyForm.css'
+
+function CurrencyForm() {
+    const [currency, setCurrency] = useState('£');
+
+    const handleCurrencyChange = (e) => {
+        const newCurrency = e.target.value;
+        setCurrency(newCurrency);
+    };
+
+    return (
+        <>
+            <form className="currencyForm">
+                <label htmlFor="currency">Choose a currency:</label>
+                <select id="currency" name="currency" value={currency} onChange={handleCurrencyChange}>
+                    <option value="£">£ Pound</option>
+                    <option value="$">$ Dollar</option>
+                    <option value="€">€ Euro</option>
+                    <option value="₹">₹ Rupee</option>
+                </select>
+            </form>
+        </>
+    );
+}
+
+export default CurrencyForm;
